94b53858995a6de4e3643f421083fe840e16b04c
bushra1-sam <s12505792@stu.najah.edu>
2026-09-18 20:19:28 +0300

    Change output message to 'Hnnnnnnnnnnnnnnnnnnnnn'

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -5,7 +5,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   
+        std::cout << "Hnnnnnnnnnnnnnnnnnnnnn\n";
+std::cout << "Hello World!\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
